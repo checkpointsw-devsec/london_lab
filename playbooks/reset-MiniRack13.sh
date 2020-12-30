9c0422e1f97de4906e5ae23c4c870a7f550a8906
chkp-tom <75310707+chkp-tom@users.noreply.github.com>
2020-12-30 15:28:11 +0000

    Removed old Mini Rack reset scripts

--- playbooks/reset-MiniRack13.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-source ../../creds.file
-echo "reset MiniRack13-MHO1"
-ansible-playbook MHO-reset.yml -e "target=MiniRack13-MHO1"
-echo "reset MiniRack13-MHO2"
-ansible-playbook MHO-reset.yml -e "target=MiniRack13-MHO2"
--- playbooks/reset-MiniRack5.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-source ../../creds.file
-echo "reset MiniRack5-MHO1"
-ansible-playbook MHO-reset.yml -e "target=MiniRack5-MHO1"
-echo "reset MiniRack5-MHO2"
-ansible-playbook MHO-reset.yml -e "target=MiniRack5-MHO2"
